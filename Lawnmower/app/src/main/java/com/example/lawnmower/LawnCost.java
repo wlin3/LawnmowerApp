5a6b9cb396c44d5f2f9c962c498aa53db3468b72
William Lin <02william.lin@gulllakecs.org>
2021-05-13 10:59:24 -0400

    Added Image and Cost Calculator

---
+++ Lawnmower/app/src/main/java/com/example/lawnmower/LawnCost.java
@@ -0,0 +1,15 @@
+package com.example.lawnmower;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LawnCost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lawn_cost);
+    }
+
+}
--- Lawnmower/app/src/main/java/com/example/lawnmower/MainActivity.java
+++ Lawnmower/app/src/main/java/com/example/lawnmower/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.lawnmower;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void Lawn(View view) {
+        Intent i = new Intent(this,LawnCost.class);
+        startActivity(i);
     }
 }
