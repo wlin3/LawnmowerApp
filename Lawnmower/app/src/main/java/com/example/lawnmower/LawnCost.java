7e869c6e55f8bdf29f2d61d62b77d0fb3b5ffbf0
William Lin <02william.lin@gulllakecs.org>
2021-05-14 11:06:30 -0400

    Added New Screens

--- Lawnmower/app/src/main/java/com/example/lawnmower/LawnCost.java
+++ Lawnmower/app/src/main/java/com/example/lawnmower/LawnCost.java
@@ -2,7 +2,9 @@ package com.example.lawnmower;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LawnCost extends AppCompatActivity {
 
@@ -11,5 +13,16 @@ public class LawnCost extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lawn_cost);
     }
-
+    public void Rectangle(View view) {
+        Intent i = new Intent(this,RectangleLawn.class);
+        startActivity(i);
+    }
+    public void Circle(View view) {
+        Intent i = new Intent(this,CircleLawn.class);
+        startActivity(i);
+    }
+    public void Triangle(View view) {
+        Intent i = new Intent(this,TriangleLawn.class);
+        startActivity(i);
+    }
 }
---
+++ Lawnmower/app/src/main/java/com/example/lawnmower/TriangleLawn.java
@@ -0,0 +1,14 @@
+package com.example.lawnmower;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TriangleLawn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_triangle_lawn);
+    }
+}
