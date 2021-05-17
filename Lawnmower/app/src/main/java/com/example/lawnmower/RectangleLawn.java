0a4ce58552a55f7403d8131d35edb05e6bb56c63
William Lin <02william.lin@gulllakecs.org>
2021-05-17 11:05:42 -0400

    Added Java Code

--- Lawnmower/app/src/main/java/com/example/lawnmower/RectangleLawn.java
+++ Lawnmower/app/src/main/java/com/example/lawnmower/RectangleLawn.java
@@ -3,12 +3,24 @@ package com.example.lawnmower;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RectangleLawn extends AppCompatActivity {
 
+    EditText length;
+    EditText width;
+    Button calculate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rectangle_lawn);
+
+        width = findViewById(R.id.width);
+        length = findViewById(R.id.length);
+
+        }
     }
+
+
 }
