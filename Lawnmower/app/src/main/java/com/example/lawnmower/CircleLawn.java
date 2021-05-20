d5cc84c0c1477fccb49e0b59be268eff3d03d074
William Lin <02william.lin@gulllakecs.org>
2021-05-20 11:16:05 -0400

    Finished Square Lawn

--- Lawnmower/app/src/main/java/com/example/lawnmower/CircleLawn.java
+++ Lawnmower/app/src/main/java/com/example/lawnmower/CircleLawn.java
@@ -2,13 +2,42 @@ package com.example.lawnmower;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class CircleLawn extends AppCompatActivity {
 
+    private Button Multiply;
+    private EditText Radius;
+    private TextView Total;
+
+    public void back(View view) {
+        Intent i = new Intent(this,LawnCost.class);
+        startActivity(i);
+    }
+    Float square = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_circle_lawn);
-    }
-}
+
+        Multiply = findViewById(R.id.calculate);
+        Radius = findViewById(R.id.radius);
+        Total = findViewById(R.id.Total);
+
+        Multiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int inputValue= Integer.parseInt(Radius.getText().toString());
+                square=inputValue*inputValue*Math.PI;
+            }
+
+        });
+
+    }}
--- Lawnmower/app/src/main/java/com/example/lawnmower/RectangleLawn.java
+++ Lawnmower/app/src/main/java/com/example/lawnmower/RectangleLawn.java
@@ -2,25 +2,47 @@ package com.example.lawnmower;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class RectangleLawn extends AppCompatActivity {
 
-    EditText length;
-    EditText width;
-    Button calculate;
+    private Button Multiply;
+    private EditText Width;
+    private EditText Length;
+    private TextView Total;
+
+    public void back(View view) {
+        Intent i = new Intent(this,LawnCost.class);
+        startActivity(i);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rectangle_lawn);
 
-        width = findViewById(R.id.width);
-        length = findViewById(R.id.length);
+        Multiply = findViewById(R.id.calculate);
+        Width = findViewById(R.id.width);
+        Length = findViewById(R.id.length);
+        Total = findViewById(R.id.Total);
 
-        }
-    }
+        Multiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double a,b,c;
+                a= Double.parseDouble(Width.getText() .toString());
+                b= Double.parseDouble(Width.getText() .toString());
+                c= a*b;
+                Total.setText(Double.toString(c));
+            }
 
+         });
 
-}
+}}
